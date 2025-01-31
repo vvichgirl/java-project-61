6ede8290acd7915f30ae3dd39eddfecce14b9bff
Viktoriia Zubkova <vichgirl@gmail.com>
2025-01-31 08:57:13 +0300

    add welcome message

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 class Cli {
     public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
