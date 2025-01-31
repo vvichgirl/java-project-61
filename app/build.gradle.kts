d6c4da3af46c32551d0a773f8a2c300b3e81c5be
Viktoriia Zubkova <vichgirl@gmail.com>
2025-01-31 08:38:24 +0300

    delete tests dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     application
     checkstyle
@@ -10,15 +9,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 application {
